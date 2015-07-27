387fd9682637922ab67b713561d8baabf38ec9e5
Scott Walsh <scottw13@gmail.com>
2015-07-27 13:41:47 -0500

    Update tests to test volume argument.

--- test/test_sensitivity/test_chooseQoIs.py
+++ test/test_sensitivity/test_chooseQoIs.py
@@ -18,6 +18,32 @@ class ChooseQoIsMethods:
     """
     Test :module:`bet.sensitivity.chooseQoIs`.
     """
+    def test_calculate_avg_condnum(self):
+        """
+        Test :meth:`bet.sensitivity.chooseQoIs.calculate_avg_condnum`.
+        """
+        self.qoi_set = range(0, self.Lambda_dim)
+        (self.condnum, self.singvals) = cQoIs.calculate_avg_condnum(self.G,
+            self.qoi_set)
+
+        # Check that condnum and singvals are the right size
+        self.assertEqual(isinstance(self.condnum, float), True)
+        self.assertEqual(self.singvals.shape, (self.num_centers,
+            self.Lambda_dim))
+
+    def test_calculate_avg_volume(self):
+        """
+        Test :meth:`bet.sensitivity.chooseQoIs.calculate_avg_volume`.
+        """
+        self.qoi_set = range(0, self.Lambda_dim)
+        (self.volume, self.singvals) = cQoIs.calculate_avg_volume(self.G,
+            self.qoi_set)
+
+        # Check that condnum and singvals are the right size
+        self.assertEqual(isinstance(self.volume, float), True)
+        self.assertEqual(self.singvals.shape, (self.num_centers,
+            self.Lambda_dim))
+
     def test_chooseOptQoIs(self):
         """
         Test :meth:`bet.sensitivity.chooseQoIs.chooseOptQoIs`.
@@ -25,23 +51,39 @@ class ChooseQoIsMethods:
         self.qoiIndices = range(0, self.num_qois)
         self.condnum_indices_mat = cQoIs.chooseOptQoIs(self.G, self.qoiIndices,
             self.num_qois_return, self.num_optsets_return)
+        self.condnum_indices_mat_vol = cQoIs.chooseOptQoIs(self.G,
+            self.qoiIndices, self.num_qois_return, self.num_optsets_return,
+            volume=True)
 
         # Test the method returns the correct size array
         self.assertEqual(self.condnum_indices_mat.shape,
             (self.num_optsets_return, self.num_qois_return + 1))
 
-        # Check that the 'global condidtion number' is greater than or equal to 1
+        self.assertEqual(self.condnum_indices_mat_vol.shape,
+            (self.num_optsets_return, self.num_qois_return + 1))
+
+        # Check that the 'global condition number' is greater than or equal to 1
         nptest.assert_array_less(1.0, self.condnum_indices_mat[:, 0])
+
+        # For voluem, check that it is greater than or equal to 0
+        nptest.assert_array_less(0.0, self.condnum_indices_mat_vol[:, 0])
 
         # Test the method returns the known best set of QoIs  (chosen to be
         # last Lambda_dim indices)
         nptest.assert_array_less(self.num_qois-self.Lambda_dim-1,
             self.condnum_indices_mat[0, 1:])
 
+        nptest.assert_array_less(self.num_qois-self.Lambda_dim-1,
+            self.condnum_indices_mat_vol[0, 1:])
+
         # Test that none of the best chosen QoIs are the same
         self.assertEqual(len(np.unique(self.condnum_indices_mat[0, 1:])),
             len(self.condnum_indices_mat[0, 1:]))
 
+        self.assertEqual(len(np.unique(self.condnum_indices_mat[0, 1:])),
+            len(self.condnum_indices_mat_vol[0, 1:]))
+
+        ##########
         # Test the method for a set of QoIs rather than all possible.  Choose
         # this set so that the optimal choice is not removed.
         self.qoiIndices = np.concatenate([range(1, 3, 2),
@@ -49,22 +91,37 @@ class ChooseQoIsMethods:
         self.condnum_indices_mat = cQoIs.chooseOptQoIs(self.G, self.qoiIndices,
             self.num_qois_return, self.num_optsets_return)
 
+        self.condnum_indices_mat_vol = cQoIs.chooseOptQoIs(self.G,
+            self.qoiIndices, self.num_qois_return, self.num_optsets_return,
+            volume=True)
+
         # Test the method returns the correct number of qois
         self.assertEqual(self.condnum_indices_mat.shape,
+            (self.num_optsets_return, self.num_qois_return + 1))
+
+        self.assertEqual(self.condnum_indices_mat_vol.shape,
             (self.num_optsets_return, self.num_qois_return + 1))
 
         # Check that the 'global condidtion number' is greater than or equal
         # to 1
         nptest.assert_array_less(1.0, self.condnum_indices_mat[:, 0])
 
+        nptest.assert_array_less(0.0, self.condnum_indices_mat_vol[:, 0])
+
         # Test the method returns the known best set of QoIs  (chosen to be
         # last Lambda_dim indices)
         nptest.assert_array_less(self.num_qois-self.Lambda_dim-1,
             self.condnum_indices_mat[0, 1:])
 
+        nptest.assert_array_less(self.num_qois-self.Lambda_dim-1,
+            self.condnum_indices_mat_vol[0, 1:])
+
         # Test that none of the best chosen QoIs are the same
         self.assertEqual(len(np.unique(self.condnum_indices_mat[0, 1:])),
             len(self.condnum_indices_mat[0, 1:]))
+
+        self.assertEqual(len(np.unique(self.condnum_indices_mat[0, 1:])),
+            len(self.condnum_indices_mat_vol[0, 1:]))
 
     def test_chooseOptQoIs_verbose(self):
         """
@@ -103,6 +160,7 @@ class ChooseQoIsMethods:
         best_sets = cQoIs.chooseOptQoIs_large(self.G, qoiIndices=self.qoiIndices,
             inner_prod_tol=self.inner_prod_tol, cond_tol=self.cond_tol)
 
+        # Test that the best_sets have conditiobn number less than the tolerance
         for Ldim in range(self.Lambda_dim - 1):
             nptest.assert_array_less(best_sets[Ldim][:, 0], self.cond_tol)
 
@@ -114,11 +172,14 @@ class ChooseQoIsMethods:
         [best_sets, optsingvals_list] = cQoIs.chooseOptQoIs_large_verbose(self.G,
             qoiIndices=self.qoiIndices, num_optsets_return=self.num_optsets_return,
             inner_prod_tol=self.inner_prod_tol, cond_tol=self.cond_tol)
-        print optsingvals_list[0].shape
 
+        # Test that Lambda_dim - 1 optsingval tensors are returned
         self.assertEqual(len(optsingvals_list), self.Lambda_dim - 1)
+
+        # Test that each tensor is the right shape
         for i in range(self.Lambda_dim - 1):
-            self.assertEqual(optsingvals_list[i].shape, (self.num_centers, i + 2, self.num_optsets_return))
+            self.assertEqual(optsingvals_list[i].shape, (self.num_centers,
+                i + 2, self.num_optsets_return))
 
 
 class test_2to20_choose2(ChooseQoIsMethods, unittest.TestCase):
